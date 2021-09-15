67b333c906827fda1ae6ce8d7cc3e32838a0fa18
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-09-15 14:45:16 -0500

    Initial commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,3 @@
+petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+withSal = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam',]
+petsNames.push('Sal')
