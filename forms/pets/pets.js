b3cd3ba491d2917497b0e65e40679b260d1d4add
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-09-15 15:00:33 -0500

    final results

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,3 +1,9 @@
 petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
-withSal = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam',]
-petsNames.push('Sal')
+withSal = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam', 'Sal']
+
+console.log(`The third pet is named ${petNames[2]}`)
+console.log(`The array has ${petNames.length} names in it`)
+
+petNames.pop()
+console.log(petNames)
+
